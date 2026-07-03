0155e044f59e584a33a0832a1e730253b7565414
HMXPANEL <hmxpanel@users.noreply.github.com>
2026-07-03 15:37:45 +0000

    Switch to image_gallery_saver_plus v4 for SDK compat

--- lib/core/utils/download_helper.dart
+++ lib/core/utils/download_helper.dart
@@ -1,12 +1,12 @@
 import 'dart:io';
 import 'package:dio/dio.dart';
-import 'package:gal/gal.dart';
+import 'package:image_gallery_saver_plus/image_gallery_saver_plus.dart';
 import 'package:path_provider/path_provider.dart';
 
 Future<void> saveImageToGallery(String url) async {
   final dir = await getTemporaryDirectory();
   final path = '${dir.path}/${DateTime.now().millisecondsSinceEpoch}.jpg';
   await Dio().download(url, path);
-  await Gal.putImage(path);
+  await ImageGallerySaverPlus.saveFile(path);
   await File(path).delete();
 }
