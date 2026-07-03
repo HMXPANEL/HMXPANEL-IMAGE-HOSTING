239148f68e2a5235419f7e3453decd85e911e6cf
HMXPANEL <hmxpanel@users.noreply.github.com>
2026-07-03 15:25:17 +0000

    Switch to image_gallery_saver_plus for AGP 8 compat

--- lib/core/utils/download_helper.dart
+++ lib/core/utils/download_helper.dart
@@ -1,6 +1,6 @@
 import 'dart:io';
 import 'package:dio/dio.dart';
-import 'package:image_gallery_saver/image_gallery_saver.dart';
+import 'package:image_gallery_saver_plus/image_gallery_saver_plus.dart';
 import 'package:path_provider/path_provider.dart';
 
 Future<void> saveImageToGallery(String url) async {
